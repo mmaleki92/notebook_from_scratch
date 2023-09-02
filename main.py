9e06779ccd0f1e607dd3b12cc823936e54c665bd
Morteza Maleki <maleki.morteza92@gmail.com>
2023-09-02 11:47:47 +0430

    added edit note command

--- main.py
+++ main.py
@@ -9,9 +9,21 @@ message = """
 4 --> delete a note
 5 --> save to json
 """
-print(message)
-command = input('1/2/3/4\n')
 note_id = 0
+while True:
+    print(message, flush=True)
+    command = input('1/2/3/4/5\n')
+    if command == '1':
+        note = {}
+        note['title'] = input('title:') 
+        note['text'] = input('text:')
+        notebook[note_id]  = note
+    elif command == '2':
+        note_id_edit = int(input('give me an id'))
+        if note_id_edit in notebook:
+            notebook[note_id_edit]['title'] = input('title:')
+            notebook[note_id_edit]['text'] = input('text:')
     elif command=='5':
         with open('notebook.json', 'w') as f:
             json.dump(notebook, f)
+    note_id += 1
