f873f0916be93f6d911edde96d1f30d1acce3456
Morteza Maleki <maleki.morteza92@gmail.com>
2023-09-02 12:36:19 +0430

    made it functional

--- main.py
+++ main.py
@@ -8,18 +8,17 @@ message = """
 4 --> delete a note
 5 --> save to json
 """
-note_id = 0
-while True:
-    if not os.path.exists('notebook.json'):
+def create_notebook(nb_file_name):
+    if not os.path.exists(nb_file_name):
         notebook = {}
-        with open('notebook.json', 'w') as f:
+        with open(nb_file_name, 'w') as f:
             json.dump(notebook, f)
     else:
-        with open('notebook.json', 'r') as f:
+        with open(nb_file_name, 'r') as f:
             notebook = json.load(f)
-    print(notebook)
-    print(message)
-    command = input('1/2/3/4/5\n')
+    return notebook
+
+def command_center(note_id, command, nb_file_name):
     if command == '1':
         note = {}
         note['title'] = input('title:') 
@@ -31,13 +30,29 @@ while True:
             notebook[note_id_edit]['title'] = input('title:')
             notebook[note_id_edit]['text'] = input('text:')
     elif command == '3':
-        print(notebook)
+        show_notes(notebook)
     elif command == '4':
         delete_id = input('give me the id to delete')
         sure = input('are you sure? (y/n)')
         if sure == 'y':
             del notebook[delete_id]
     elif command=='5':
-        with open('notebook.json', 'w') as f:
+        with open(nb_file_name, 'w') as f:
             json.dump(notebook, f)
+def show_notes(notebook):
+    for id in notebook.keys():
+        print('-'*10)
+        print('Title:', notebook[id]['title'])
+        print('Text:', notebook[id]['text'][0:10])
+        print('-'*10)
+
+note_id = 0
+nb_file_name = 'notebook.json'
+while True:
+    notebook = create_notebook(nb_file_name)
+    show_notes(notebook)
+    print(message)
+    command = input('1/2/3/4/5\n')
+    command_center(note_id, command, nb_file_name)
+   
     note_id += 1
